18b25bc08b5f900ca87046ca4d068eea05ddcdc4
Lukaslalas <Lukaslalas10@gmail.com>
2022-05-01 22:08:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.graphics.Typeface;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
+    Button upButton, b1;
+    Float diff=2.0f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.b1 =(Button)findViewById(R.id.button1);
+        this.tvMain = (TextView) findViewById(R.id.tvMain); //this kviecia globalu
+        this.tvMain.setText("Pagrindinis langas - Labas");
+        this.upButton = findViewById(R.id.up);
+    }
+
+    public void onBtnMainClick(View view) {
+        this.tvMain.setText("Paspausta");
+    }
+
+    public void onBtnMain2Color(View view) {
+        tvMain.setTextColor(Color.rgb(200,240,200));
+
+    }
+    public void onBtnMainSize (View view) {
+        tvMain.setTextSize(tvMain.getTextSize() + diff);
+    }
+    public void onBtnMainStyle(View view) {
+        tvMain.setTypeface(Typeface.MONOSPACE);
+    }
+
+}
+
